79f936463cef711ec8a8ae5309a05675f4a4bfc8
suhyuncc <suhyuncc@gmail.com>
2025-12-11 23:19:26 +0900

    put: stage, deck building_UI
    - 덱빌딩 UI 넣기

---
+++ Assets/Scripts/UI/Stage_Select/Stage_BookBtn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Stage_BookBtn : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (StageScene_UIManager.Instance != null)
+            StageScene_UIManager.Instance.Light_On();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (StageScene_UIManager.Instance != null)
+            StageScene_UIManager.Instance.Light_Off();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
