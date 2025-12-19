929da1a2756c510f731fb4479e34b7fd0cef0eef
suhyuncc <suhyuncc@gmail.com>
2025-12-19 19:23:08 +0900

    feat: sound

---
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private List<SoundData> soundDatas;
+
+    private Dictionary<SoundID, SoundData> _soundMap;
+
+    private AudioSource _bgmSource;
+    private List<AudioSource> _sfxPool = new();
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        _bgmSource = gameObject.AddComponent<AudioSource>();
+
+        _soundMap = new Dictionary<SoundID, SoundData>();
+        foreach (var data in soundDatas)
+            _soundMap[data.id] = data;
+    }
+
+    public void Play(SoundID id)
+    {
+        if (!_soundMap.TryGetValue(id, out SoundData data))
+            return;
+
+        switch (data.type)
+        {
+            case SoundType.BGM:
+                PlayBGM(data);
+                break;
+            case SoundType.SFX:
+            case SoundType.UI:
+                PlaySFX(data);
+                break;
+        }
+    }
+
+    void PlayBGM(SoundData data)
+    {
+        if (_bgmSource.clip == data.clip)
+            return;
+
+        _bgmSource.clip = data.clip;
+        _bgmSource.volume = data.volume;
+        _bgmSource.loop = true;
+        _bgmSource.Play();
+    }
+
+    void PlaySFX(SoundData data)
+    {
+        AudioSource source = GetAvailableSource();
+        source.clip = data.clip;
+        source.volume = data.volume;
+        source.pitch = Random.Range(
+            data.pitch - 0.05f,
+            data.pitch + 0.05f
+        );
+
+        source.loop = false;
+        source.Play();
+    }
+
+    AudioSource GetAvailableSource()
+    {
+        foreach (var src in _sfxPool)
+            if (!src.isPlaying)
+                return src;
+
+        var newSource = gameObject.AddComponent<AudioSource>();
+        _sfxPool.Add(newSource);
+        return newSource;
+    }
+
+    public void SetBGMVolume(float value)
+    {
+        _bgmSource.volume = value;
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        foreach (var src in _sfxPool)
+            src.volume = value;
+    }
+}
