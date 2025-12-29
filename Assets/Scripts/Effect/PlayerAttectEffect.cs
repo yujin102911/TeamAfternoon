b5eb5f502f6c3f8f80275a351bc0f567f5fd4648
summer-yeoleum <indeedselfharm@gmail.com>
2025-12-29 14:16:34 +0900

    feat/활 차징+활 차징 이펙트

--- Assets/Scripts/Effect/PlayerAttectEffect.cs
+++ Assets/Scripts/Effect/PlayerAttectEffect.cs
@@ -4,17 +4,27 @@ public class PlayerAttectEffect : MonoBehaviour
 {
     [SerializeField] private GameObject swordAttackFX;
     [SerializeField] private GameObject bowAttackFX;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private GameObject bowChargingFX;
+
+    [SerializeField] private Vector3 attackOffset;
+    [SerializeField] private Vector3 chargingOffset;
+
 
     public void SpawnSwordAttackEffect()
     {
-        Vector3 spawnPos = offset;
+        Vector3 spawnPos = attackOffset;
         Instantiate(swordAttackFX, spawnPos, Quaternion.identity);
     }
 
     public void SpawnBowAttackEffect()
     {
-        Vector3 spawnPos = offset;
+        Vector3 spawnPos = attackOffset;
         Instantiate(bowAttackFX, spawnPos, Quaternion.identity);
+    }
+
+    public void SpawnBowChargingEffect()
+    {
+        Vector3 spawnPos = chargingOffset;
+        Instantiate(bowChargingFX, spawnPos, Quaternion.identity);
     }
 }
