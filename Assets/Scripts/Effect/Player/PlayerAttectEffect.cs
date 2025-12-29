5a0efacd9622fa71fada70226551d49239d79201
suhyuncc <suhyuncc@gmail.com>
2025-12-29 23:01:23 +0900

    feat: shoot

--- Assets/Scripts/Effect/Player/PlayerAttectEffect.cs
+++ Assets/Scripts/Effect/Player/PlayerAttectEffect.cs
@@ -9,6 +9,7 @@ public class PlayerAttectEffect : MonoBehaviour
     [SerializeField] private Vector3 attackOffset;
     [SerializeField] private Vector3 chargingOffset;
 
+    private Animator _currentAnimtor;
 
     public void SpawnSwordAttackEffect()
     {
@@ -22,12 +23,30 @@ public class PlayerAttectEffect : MonoBehaviour
         var fx = bowAttackFX_pool.GetEffect();
         fx.transform.position = attackOffset;
         fx.SetActive(true);
+
+        if (_currentAnimtor != null)
+        {
+            _currentAnimtor.SetTrigger("Shoot");
+            _currentAnimtor = null;
+        }
     }
 
     public void SpawnBowChargingEffect()
     {
-        var fx = bowChargingFX_pool.GetEffect();
-        fx.transform.position = transform.position + chargingOffset;
-        fx.SetActive(true);
+        if(_currentAnimtor != null)
+        {
+            _currentAnimtor.SetTrigger("Charging_Lv2");
+
+        }
+        else
+        {
+            var fx = bowChargingFX_pool.GetEffect();
+            //Vector3 worldPos = transform.TransformPoint(transform.position);
+            fx.transform.position = transform.position + chargingOffset;
+            fx.SetActive(true);
+            _currentAnimtor = fx.GetComponent<Animator>();
+        }
+
+            
     }
 }
