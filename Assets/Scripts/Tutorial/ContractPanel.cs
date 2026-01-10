72bf894ef14ef205eee912fdb595447ebd3e6c8e
박유진 <yujin102911@gmail.com>
2026-01-10 22:09:00 +0900

    p11 까지 완성

--- Assets/Scripts/Tutorial/ContractPanel.cs
+++ Assets/Scripts/Tutorial/ContractPanel.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ContractPanel : MonoBehaviour
 {
@@ -7,15 +9,19 @@ public class ContractPanel : MonoBehaviour
     [SerializeField] private Toggle _yesToggle;
     [SerializeField] private Toggle _noToggle;
     [SerializeField] private Button _finishButton;
+    [SerializeField] private IntroPanel _introPanel;
 
     [Header("씬 이름 설정")]
-    [SerializeField] private string _tutorialSceneName = "TutorialScene";
     [SerializeField] private string _mainSceneName = "MainScene";
 
     [Header("아니오일때의 Popup")]
     [SerializeField] private GameObject _realPopup;
     [SerializeField] private Button _passButton;
     [SerializeField] private Button _goTutorialButton;
+    [SerializeField] private List<int> _addBlocks;
+
+    [Header("Nail 설치마법사 Panel")]
+    [SerializeField] private GameObject _nailPanel;
 
     private void Awake()
     {
@@ -39,13 +45,31 @@ public class ContractPanel : MonoBehaviour
         }
     }
 
+    public void GoTutorial()
+    {
+        StartCoroutine(GoTutorialRoutine());
+    }
+
     /// <summary>
     /// 어떤 방식이든 튜토리얼을 한다고 선택했을 때 실행되는 로직
     /// </summary>
-    private void GoTutorial()
+    private IEnumerator GoTutorialRoutine()
     {
         Debug.Log("튜토리얼을 시작합니다.");
-        // 튜토리얼 시작 로직 (저장 X)
+        ServiceLocator.Instance.Cursor.StartAnimation("Loading");
+        CanvasGroup cg = GetComponent<CanvasGroup>();
+        if (cg != null)
+        {
+            cg.alpha = 0f;
+            cg.blocksRaycasts = false;
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        ServiceLocator.Instance.Cursor.StopAnimation();
+        _nailPanel.SetActive(true);
+        gameObject.SetActive(false);
+
     }
 
     /// <summary>
@@ -54,8 +78,33 @@ public class ContractPanel : MonoBehaviour
     private void GoWithoutTutorial()
     {
         Debug.Log("튜토리얼을 진행하지 않고 게임을 시작합니다.");
-        ServiceLocator.Instance.SaveNowUserData(); // 새로운 게임 데이터 생성
-        // 추가로 UserData의 Tutorial클리어 처리 여부도 여기서 결정
+        if (ServiceLocator.Instance.CurrentUser != null && _addBlocks != null)
+        {
+            foreach (int block in _addBlocks)
+            {
+                ServiceLocator.Instance.CurrentUser.AddUnlockedBlock(block);
+            }
+        }
+        ServiceLocator.Instance.SaveNowUserData(); // 새로운 게임 데이터 저장
+        StartCoroutine(End());
+        //ServiceLocator.Instance.Scene.Load(_mainSceneName);
+        // 추가로 UserData의 Tutorial클리어 처리 여부도 여기서 결정 + block 추가도 여기서 ~.~
+    }
+
+    private IEnumerator End()
+    {
+        AsyncOperation asyncLoad = null;
+        if (ServiceLocator.Instance != null && ServiceLocator.Instance.Scene != null)
+        {
+            asyncLoad = ServiceLocator.Instance.Scene.LoadAsync(_mainSceneName);
+            asyncLoad.allowSceneActivation = false;
+        }
+        yield return new WaitForSeconds(0.5f);
+        _introPanel.StartDayChange(0, 1);
+        yield return _introPanel.FadeOutRoutine();
+
+        if (asyncLoad != null)
+            asyncLoad.allowSceneActivation = true;
     }
 
 }
---
+++ Assets/Scripts/Tutorial/NailInstaller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NailInstaller : MonoBehaviour
+{
+    [SerializeField] private Button _p1Btn;
+    [SerializeField] private GameObject _p1;
+    [SerializeField] private GameObject _p2;
+    [SerializeField] private GameObject _p3;
+    [SerializeField] private Button _endBtn;
+    [SerializeField] private string _tutoSelectScene = "TutorialDesktop";
+    [SerializeField] private IntroPanel _introPanel;
+
+    private void Awake()
+    {
+        _p1Btn.onClick.AddListener(Install);
+        _endBtn.onClick.AddListener(GoTuto);
+    }
+
+
+    private void Install()
+    {
+        _p1.SetActive(false);
+        _p2.SetActive(true);
+        StartCoroutine(DownloadRoutine());
+    }
+
+    private IEnumerator DownloadRoutine()
+    {
+        ServiceLocator.Instance.Cursor.StartAnimation("Loading");
+        // TODO: 다운로드 연출 추가
+        ServiceLocator.Instance.Cursor.StopAnimation();
+        yield return null;
+        _p2.SetActive(false);
+        _p3.SetActive(true);
+    }
+
+    private void GoTuto()
+    {
+        StartCoroutine(EndButton());
+    }
+
+    // end버튼 클릭 시 나오는 연출 IEnumerator
+    private IEnumerator EndButton()
+    {
+        AsyncOperation asyncLoad = null;
+        if (ServiceLocator.Instance != null &&  ServiceLocator.Instance.Scene != null)
+        {
+            asyncLoad = ServiceLocator.Instance.Scene.LoadAsync(_tutoSelectScene);
+            asyncLoad.allowSceneActivation = false;
+        }
+        yield return new WaitForSeconds(0.5f);
+        _introPanel.StartDayChange(0, 1);
+        yield return _introPanel.FadeOutRoutine();
+
+        if (asyncLoad != null)
+            asyncLoad.allowSceneActivation = true;
+
+    }
+
+}
