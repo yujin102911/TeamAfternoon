9ad8b2c2545472b9fc6a07830ec18e1a801c5d23
박유진 <yujin102911@gmail.com>
2026-01-12 17:40:07 +0900

    [fix] 크레딧 수정

--- Assets/Scripts/NewStageSelect/CreditPanel.cs
+++ Assets/Scripts/NewStageSelect/CreditPanel.cs
@@ -7,14 +7,50 @@ public class CreditPanel : MonoBehaviour
     // 필요없으면 걍 x버튼 기능 딴데로 옮기고 삭제 ㄱㄱ
     [SerializeField] private Button _xButton;
 
+
+    [SerializeField] private GameObject _generalOb;
+    [SerializeField] private Button _general_specialBtn;
+    [SerializeField] private Button _general_thirdBtn;
+
+    [SerializeField] private GameObject _specialOb;
+    [SerializeField] private Button _special_generalBtn;
+    [SerializeField] private Button _special_thridBtn;
+
+    [SerializeField] private GameObject _thirdOb;
+    [SerializeField] private Button _third_generalBtn;
+    [SerializeField] private Button _third_specialBtn;
+
     private void Awake()
     {
         _xButton.onClick.AddListener(CloseCredit);
+
+        _special_generalBtn.onClick.AddListener(OnGeneralBtnClick);
+        _third_generalBtn.onClick.AddListener(OnGeneralBtnClick);
+
+        _general_specialBtn.onClick.AddListener(OnSpecialBtnClick);
+        _third_specialBtn.onClick.AddListener(OnSpecialBtnClick);
+
+        _general_thirdBtn.onClick.AddListener(OnThirdBtnClick);
+        _special_thridBtn.onClick.AddListener(OnThirdBtnClick);
     }
 
     private void CloseCredit()
     {
         gameObject.SetActive(false);
+    }
+
+    private void OnGeneralBtnClick()
+    {
+        _generalOb.transform.SetAsLastSibling();
+    }
+    private void OnSpecialBtnClick()
+    {
+        _specialOb.transform.SetAsLastSibling();
+    }
+
+    private void OnThirdBtnClick()
+    {
+        _thirdOb.transform.SetAsLastSibling();
     }
 
 }
