5d5f3c428c4d23e0596ae55e307b99c3b5a2c8b7
박유진 <yujin102911@gmail.com>
2026-01-17 00:46:33 +0900

    [feat] 특수 효과 알림

---
+++ Assets/Scripts/UI/InGame/SpecialEffectNotification.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpecialEffectNotification : MonoBehaviour
+{
+    [SerializeField] private GameObject _visualRoot;
+    [SerializeField] private Button _closeButton;
+
+    private void Awake()
+    {
+        if (_closeButton != null)
+        {
+            _closeButton.onClick.AddListener(Hide);
+        }
+        if (_visualRoot != null) _visualRoot.SetActive(false);
+    }
+
+    public void Show()
+    {
+        if (_visualRoot != null)
+        {
+            _visualRoot.SetActive(true);
+        }
+    }
+
+    private void Hide()
+    {
+        if (_visualRoot != null)
+        {
+            _visualRoot.SetActive(false);
+        }
+    }
+    
+
+}
