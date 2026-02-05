516fab5e9677ca840624f9452f1e24fa943550d5
박유진 <yujin102911@gmail.com>
2026-02-05 21:45:44 +0900

    [fix] week 1 추가

--- Assets/Scripts/Service/ServiceLocator.cs
+++ Assets/Scripts/Service/ServiceLocator.cs
@@ -106,7 +106,7 @@ public class ServiceLocator : MonoBehaviour
             }
             foreach (var twit in CurrentTwitData.TwitDatas)
             {
-                if (twit.Difficulty == Difficulty.Easy)
+                if (twit.Difficulty == Difficulty.Easy || (twit.Difficulty == Difficulty.Hard && twit.UploadDay == 1))
                 {
                     twit.IsVisible = true;
                 }
