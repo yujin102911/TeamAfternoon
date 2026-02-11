8eb14055acb898e6aa172bb382db58b6cef2d3bf
박유진 <yujin102911@gmail.com>
2026-02-11 18:26:28 +0900

    4칸으로 로그아웃 기능 추가

---
+++ Assets/Scripts/NewStageSelect/LogoutPanel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoutPanel : MonoBehaviour
+{
+    [SerializeField] private Button _xBtn; // 오른쪽 위 X 버튼
+    [SerializeField] private Button _noBtn; // 아니요
+    [SerializeField] private Button _yesBtn; // 네
+
+    [SerializeField] private string _titleSceneName = "";
+
+    private void Awake()
+    {
+        _xBtn.onClick.AddListener(TurnOffPanel);
+        _noBtn.onClick.AddListener(TurnOffPanel);
+
+        _yesBtn.onClick.AddListener(ChangeScene);
+    }
+
+    public void TurnOffPanel()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ChangeScene()
+    {
+        Debug.Log("저장");
+        SaveService.Save(ServiceLocator.Instance.CurrentUser);
+        TwitSaveService.Save(ServiceLocator.Instance.CurrentTwitData);
+        ServiceLocator.Instance.Scene.Load(_titleSceneName);
+    }
+
+}
