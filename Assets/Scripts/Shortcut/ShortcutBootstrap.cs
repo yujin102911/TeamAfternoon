839f488c26da6a136e62c8077efb7e1fe64abcc4
suhyuncc <suhyuncc@gmail.com>
2026-02-11 15:56:44 +0900

    단축키 지정

---
+++ Assets/Scripts/Shortcut/ShortcutBootstrap.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShortcutBootstrap : MonoBehaviour
+{
+    [SerializeField] private ShortcutManager _shortcuts;
+    [SerializeField] private ScrollRect _handScroll;
+    [SerializeField] private ScrollRect _effectScroll;
+
+    [Header("필터 토글")]
+    [SerializeField] private Toggle[] _filterToggles;
+
+    [Header("최적화 조건")]
+    [SerializeField]
+    private GameObject _qeustPanel;
+    [SerializeField]
+    private GameObject _qeustBtn;
+
+    [Header("패널 버튼")]
+    [SerializeField]
+    private Button _handPanelBtn;
+    [SerializeField]
+    private Button _effectPanelBtn;
+    private Button _currentBtn;
+    private void Awake()
+    {
+        _shortcuts.Register(new ScrollMoveCommand("ui.scroll.up", GetActiveScroll, +1000.0f));
+
+        _shortcuts.Register(new ScrollMoveCommand("ui.scroll.down", GetActiveScroll, -1000.0f));
+        // 여기서 다른 흩어진 기능들도 다 Register로 모음
+        _shortcuts.Register(new ActionCommand(
+            "ui.qeust",
+            QeustPanel_Toggle
+        ));
+
+        _shortcuts.Register(new ActionCommand(
+            "ui.tab",
+            Panel_Toggle
+        ));
+
+        for (int i = 0; i < _filterToggles.Length; i++)
+        {
+            int index = i;
+
+            _shortcuts.Register(
+                new ToggleFilterCommand(
+                    $"filter.toggle.{index}",
+                    _filterToggles[index]
+                )
+            );
+        }
+    }
+
+    private void QeustPanel_Toggle()
+    {
+        if (GameManager.Instance != null && GameManager.Instance.IsBattleEnded)
+        {
+            return;
+        }
+
+        bool isActive = _qeustPanel.activeSelf;
+        _qeustPanel.SetActive(!isActive);
+        _qeustBtn.SetActive(isActive);
+    }
+
+    private void Panel_Toggle()
+    {
+        if (GameManager.Instance != null && (!GameManager.Instance.IsGameStarted || GameManager.Instance.IsBattleEnded))
+        {
+            return;
+        }
+
+        if (_handPanelBtn.gameObject.activeInHierarchy)
+            _handPanelBtn.onClick.Invoke();
+        else
+            _effectPanelBtn.onClick.Invoke();
+    }
+
+    private ScrollRect GetActiveScroll()
+    {
+        if (_handScroll.gameObject.activeInHierarchy)
+            return _handScroll;
+
+        if (_effectScroll.gameObject.activeInHierarchy)
+            return _effectScroll;
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/Shortcut/ToggleFilterCommand.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+
+public class ToggleFilterCommand : IShortcutCommand
+{
+    public string Id { get; }
+
+    private readonly Toggle _toggle;
+
+    public ToggleFilterCommand(string id, Toggle toggle)
+    {
+        Id = id;
+        _toggle = toggle;
+    }
+
+    public bool CanExecute()
+        => _toggle != null && _toggle.gameObject.activeInHierarchy;
+
+    public void Execute(float dt)
+    {
+        if (GameManager.Instance != null && !GameManager.Instance.IsGameStarted || GameManager.Instance.IsBattleEnded)
+        {
+            return;
+        }
+
+        _toggle.isOn = !_toggle.isOn;
+    }
+}
