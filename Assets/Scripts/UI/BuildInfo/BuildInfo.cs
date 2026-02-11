d00ab6d7829d54ac49e086e0fd07a510dfece35a
suhyuncc <suhyuncc@gmail.com>
2026-02-11 18:26:39 +0900

    ㅔㄷ

---
+++ Assets/Scripts/UI/BuildInfo/BuildInfo.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class BuildInfo : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    void Start()
+    {
+        if (text != null)
+            text.text = GetBuildInfo();
+    }
+
+    string GetBuildInfo()
+    {
+        return
+            $"Version: {Application.version}\n" +
+            $"Unity: {Application.unityVersion}\n" +
+            $"Platform: {Application.platform}\n";
+    }
+}
